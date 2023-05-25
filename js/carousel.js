69d7b481c9a49ffaf691a391e788c4a77f963e8b
Umrzoq0323 <araqulovumrzoq0323@gmail.com>
2023-05-25 05:16:12 +0500

    tayyor

---
+++ js/carousel.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        500:{
+            item:1,
+        },
+        768:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
