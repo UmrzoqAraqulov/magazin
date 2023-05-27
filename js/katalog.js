1ac1e51a4d7cfd76963c73e4e496f1cb6fd95f18
Umrzoq0323 <araqulovumrzoq0323@gmail.com>
2023-05-27 08:18:55 +0500

    end

---
+++ js/katalog.js
@@ -0,0 +1,12 @@
+let bar = document.querySelector(".tovarlar");
+
+let ktg = document.querySelector(".bar .icon");
+let ktg1 = document.querySelector(".bar1 i");
+
+ktg.addEventListener("click",function(){
+    bar.classList.toggle("tv");
+})
+
+ktg1.addEventListener("click",function(){
+    bar.classList.toggle("tv");
+})
